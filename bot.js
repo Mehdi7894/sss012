5e56d2c52c86522b709e249304c92430c6a45a64
Mehdi7894 <47418152+Mehdi7894@users.noreply.github.com>
2019-02-23 18:25:01 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,7 +27,7 @@ const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
 const sql = require("sqlite");
 const dateFormat = require('dateformat'); 
 const pretty = require('pretty-ms') 
-const prefix = 'G.';
+const prefix = '$';
 var table = require('table').table
 var ti={}  
 ,spee={}
@@ -35,7 +35,7 @@ var ti={}
 
 client.on('ready', function(){
     var ms = 60000 ;
-    var setGame = [`${client.guilds.size} Server`,'-help','Type -help',`${client.users.size} Members`,'#DS..','By: #DS.. Server'];
+    var setGame = [`${client.guilds.size} Server`,'$help','Type $help',`${client.users.size} Members`,'Huwaii','By: Huwaii,. Server'];
     var i = -1;
     var j = 0;
     setInterval(function (){
